727b527fd043a22cbe8ade0e47a31a08e34d2215
Fareeha-Naseem17 <fareeha.naseem123@gmail.com>
2024-03-30 14:46:15 -0700

    Created a number guessing game

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\tWELCOME TO CLI NUMBER GUESSING GAME\n");
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1-6: ",
+    },
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("Congratulations! You Guessed the Right Number.");
+}
+else {
+    console.log("You Guessed the Wrong number.");
+}
